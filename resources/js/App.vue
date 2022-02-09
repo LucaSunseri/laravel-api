5695b31baeb0e69dddeade05363448e5db7bc110
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2022-02-09 17:15:17 +0100

    Call all posts whit axios

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,19 +1,21 @@
 <template>
     <div>
         <Header />
-        <h1>Pagina Pubblica</h1>
+        <Posts />
         <Footer />
     </div>
 </template>
 
 <script>
 import Header from "./components/partials/Header.vue";
+import Posts from "./components/Posts.vue";
 import Footer from "./components/partials/Footer.vue";
 
 export default {
     name: "App",
     components: {
         Header,
+        Posts,
         Footer,
     },
 };
